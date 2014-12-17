ebfc4e91514215b2b41a6365b60c04dec5d0a581
Mathias Garbe <mail@mathias-garbe.de>
2014-12-17 16:19:28 +0100

    Fixes wrong capitalization of user rules

--- src/main/webapp/WEB-INF/ddl/ddl_hsql_recreate_schema.sql
+++ src/main/webapp/WEB-INF/ddl/ddl_hsql_recreate_schema.sql
@@ -13,6 +13,6 @@ alter table Member add constraint name_ unique (name);
 alter table Member_Member add constraint FK8BA0F3BF32E89F5A foreign key (friendsOf_id) references Member;
 alter table Member_Member add constraint FK8BA0F3BF32B215B foreign key (Member_id) references Member;
 
-INSERT INTO Member (email, password, role, name) VALUES ('admin@picture.com', '$2a$12$Fw0mMs5dXY6we1yv2/9jIumDy/17I8F09mjfwd7dlE2uEzjbRdwfC', 'Admin', 'a');
-INSERT INTO Member (email, password, role, name) VALUES ('user-b@picture.com', '$2a$12$.nIff9W1T/Ejh31bdLqcee/S.4Pzy6JvjgzROO1dJC9jRz4QQbgO2', 'User', 'b');
-INSERT INTO Member (email, password, role, name) VALUES ('user-c@picture.com', '$2a$12$sDQw4yyWlxRej4dkKvLgHu5GROojYb0YQNwoLfiFQ/MlZL2SX7X3.', 'User', 'c');
+INSERT INTO Member (email, password, role, name) VALUES ('admin@picture.com', '$2a$12$Fw0mMs5dXY6we1yv2/9jIumDy/17I8F09mjfwd7dlE2uEzjbRdwfC', 'admin', 'a');
+INSERT INTO Member (email, password, role, name) VALUES ('user-b@picture.com', '$2a$12$.nIff9W1T/Ejh31bdLqcee/S.4Pzy6JvjgzROO1dJC9jRz4QQbgO2', 'user', 'b');
+INSERT INTO Member (email, password, role, name) VALUES ('user-c@picture.com', '$2a$12$sDQw4yyWlxRej4dkKvLgHu5GROojYb0YQNwoLfiFQ/MlZL2SX7X3.', 'user', 'c');
